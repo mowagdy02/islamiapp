b5d782d857a9d432cf98606298aa174c3dce30e7
mhmed <mhmed.wagdy2002@gmail.com>
2026-04-28 20:57:10 +0300

    last

--- lib/radio_screen.dart
+++ lib/radio_screen.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:islamiapp/Appcolors.dart';
 import 'package:islamiapp/custom_text.dart';
 
-class RadioScreen extends StatelessWidget {
+class RadioScreen extends StatefulWidget {
   const RadioScreen({super.key});
 
+  @override
+  State<RadioScreen> createState() => _RadioScreenState();
+}
+bool iSreciter = false;
+
+class _RadioScreenState extends State<RadioScreen> {
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width ;
@@ -29,25 +35,37 @@ class RadioScreen extends StatelessWidget {
                       padding: EdgeInsets.symmetric(horizontal: width * 0.16,vertical: 8 ),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
-                        color: Appcolors.gold,
+                        color: iSreciter ? Color(0x8B121010) : Appcolors.gold,
                       ),
-                      child: CustomText(data: "Radio",size: 18,color: Appcolors.black,),
+                      child: CustomText(data: "Radio",size: 18,color: iSreciter?   Appcolors.white:Appcolors.black,),
                     ),
+                      onTap: () {
+                        iSreciter = false;
+                        setState(() {
+
+                        });
+                      },
                   ),
                   InkWell(
                     child: Container(
                       padding: EdgeInsets.symmetric(horizontal: width * 0.16,vertical: 8 ),
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
-                        color: Color(0x8B121010),
+                        color: iSreciter? Appcolors.gold : Color(0x8B121010),
                       ),
-                      child: CustomText(data: "Reciters",size: 16,color: Appcolors.white,),
+                      child: CustomText(data: "Reciters",size: 16,color: iSreciter?   Appcolors.black:Appcolors.white,),
                     ),
+                    onTap: () {
+                      iSreciter = true;
+                      setState(() {
+
+                      });
+                    },
                   ),
                 ],
               ),
               Expanded(
-                child: ListView.separated(itemBuilder: (context, index) {
+                child: iSreciter? ListView.separated(itemBuilder: (context, index) {
                   return Container(
                     width: width,
                     height: height * 0.15,
@@ -69,7 +87,33 @@ class RadioScreen extends StatelessWidget {
                       ],
                     ),
                   );
-                }, separatorBuilder: (context, index) => SizedBox(height: 20,), itemCount: 20),
+                }, separatorBuilder: (context, index) =>
+                    SizedBox(height: 20,), itemCount: 20):
+                ListView.separated(itemBuilder: (context, index) {
+                  return Container(
+                    width: width,
+                    height: height * 0.15,
+                    decoration: BoxDecoration(
+                        color: Appcolors.gold,
+                        borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: Stack(
+                      alignment: AlignmentDirectional.bottomCenter,
+                      children: [
+                        Image.asset("assets/images/Mask group.png"),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            CustomText(data: "Radio Ibrahim Al-Akdar",color: Appcolors.black,),
+                            Image.asset("assets/images/Group 8.png")
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                }, separatorBuilder: (context, index) =>
+                    SizedBox(height: 20,), itemCount: 20)
+
               )
                 ],
               ),
